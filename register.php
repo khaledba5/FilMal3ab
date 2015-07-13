9e930340062018959a860194d272d377e7a5a6c6
Khaled M.Fathy <eng.khaled.93@hotmail.com>
2015-07-13 04:43:06 +0200

    Issue #1,Adding Form

--- register.php
+++ register.php
@@ -3,7 +3,20 @@
 </head>
 
 <body>
-hey
+<div id="container">
+FilMal3ab
+<form>
+	<input type="text" name="firstname" placeholder="First Name"><br>
+	<input type="text" name="middlename" placeholder="Middle Name"><br>
+	<input type="text" name="age" placeholder="Age"><br>
+	<input type="text" name="username" placeholder="Username (Nickname)"><br>
+	<input type="password" name="password" placeholder="Password"><br>
+	<input type="password" name="repassword" placeholder="Retype Your Password"><br>
+	<input type="text" name="phone" placeholder="Mobile"><br>
+	<button>Get Started</button>
+</form>
+
+</div>
 </body>
 
 <?php
