ea93f610713f09816ee28cb7a18acf265f587f22
Khaled M.Fathy <eng.khaled.93@hotmail.com>
2015-07-15 03:09:20 +0200

    Finish Nav Bar

--- register.php
+++ register.php
@@ -1,26 +1,22 @@
+
 <head>
 	<link rel="stylesheet" type="text/css" href="css/register.css" />
+
 </head>
-
 <body>
-<div id="container">
-FilMal3ab
-<form>
-	<input type="text" name="firstname" placeholder="First Name"><br>
-	<input type="text" name="middlename" placeholder="Middle Name"><br>
-	<input type="text" name="age" placeholder="Age"><br>
-	<input type="text" name="username" placeholder="Username (Nickname)"><br>
-	<input type="password" name="password" placeholder="Password"><br>
-	<input type="password" name="repassword" placeholder="Retype Your Password"><br>
-	<input type="text" name="phone" placeholder="Mobile"><br>
-	<button>Get Started</button>
-</form>
+	<div class="nav">
+		<div class="logo">
+			<img src="images/logo.png">
+		</div>
+		<div id="login">
+			<p>Already have an account?</p>
+			<a href="#">Login</a>
+		</div>
+	</div>
 
-</div>
 </body>
 
 <?php
-
 
 
 ?>
